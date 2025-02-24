af7f1d77bd19a64feb9d498d38dc3e68276aa9f5
vijay ac <vijay@invitosoftwaresolutions.com>
2025-02-24 17:08:36 +0530

    Setup

---
+++ src/utils/fackData/menuList.js
@@ -0,0 +1,43 @@
+export const menuList = [
+    {
+        id: 0,
+        name: "dashboards",
+        path: "/",
+        icon: 'feather-airplay',
+        dropdownMenu: [
+        ]
+    },
+    {
+        id: 1,
+        name: "Users",
+        path: "#",
+        icon: 'feather-cast',
+        dropdownMenu: []
+    },
+    {
+        id: 2,
+        name: "Transactions",
+        path: '#',
+        icon: 'feather-send',
+        dropdownMenu: [
+           
+        ]
+    },
+
+    {
+        id: 9,
+        name: "settings",
+        path: "#",
+        icon: 'feather-settings',
+        dropdownMenu: [
+            {
+                id: 1,
+                name: "Ganeral",
+                path: "/settings/ganeral",
+                subdropdownMenu: false
+            },
+            
+        ]
+    },
+  
+]
